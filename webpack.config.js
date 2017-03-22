74978cec6c5d0f6667773789e583e389d1bad0a1
Robin Kuehn <kuehn.robin@gmail.com>
2017-03-21 20:46:56 -0700

    reconfigure webpack to bundle css files

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,42 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
+const combineLoaders = require('webpack-combine-loaders');
 
-var BUILD_DIR = path.resolve(__dirname, 'client/public');
-var APP_DIR = path.resolve(__dirname, 'client/app');
+const BUILD_DIR = path.resolve(__dirname, 'client/public');
+const APP_DIR = path.resolve(__dirname, 'client/app');
 
-var config = {
-  entry: APP_DIR + '/index.js',
+const config = {
+  entry: `${APP_DIR}/index.js`,
   output: {
     path: BUILD_DIR,
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   devServer: {
     inline: true,
     contentBase: './client',
     historyApiFallback: true,
-    port: 8100
+    port: 8100,
   },
-  module : {
-    loaders : [
-      {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel-loader'
-      }
-    ]
-  }
+  module: {
+    loaders: [{
+      test: /\.jsx?/,
+      include: APP_DIR,
+      loader: 'babel-loader',
+    }, {
+      test: /\.css$/,
+      loader: combineLoaders([
+        {
+          loader: 'style-loader',
+        }, {
+          loader: 'css-loader',
+          query: {
+            modules: true,
+            localIdentName: '[name]__[local]___[hash:base64:5]',
+          },
+        },
+      ]),
+    }],
+  },
 };
 
 module.exports = config;
